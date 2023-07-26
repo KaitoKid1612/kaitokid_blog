9b3a42a9084ec7f138520cec7fe491033a156513
KaitoKid1612 <leviet1612@gmail.com>
2023-07-26 17:31:05 +0700

    Use VueMixin and Axios

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,8 @@ import ViewUI from "view-design";
 import "view-design/dist/styles/iview.css";
 
 Vue.use(ViewUI);
+import common from "./common.js";
+Vue.mixin(common);
 
 // window.Vue = require("vue");
 
